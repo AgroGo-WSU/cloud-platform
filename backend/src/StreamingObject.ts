164e2055c443760b30376468090107b93bb321fc
Nicholas Kerner <114034770+ZingoZango12@users.noreply.github.com>
2025-09-17 19:09:26 -0400

    forgot to add some comments

--- backend/src/StreamingObject.ts
+++ backend/src/StreamingObject.ts
@@ -2,10 +2,13 @@ import { drizzle } from "drizzle-orm/d1";
 import * as schema from "./schema";
 import { DurableObject } from "cloudflare:workers";
 
+//connects to database
 export interface Env {
     DB: D1Database;
 }
 
+//I looked this up and have not a clue how this works just yet the old
+//way of doing this is not good anymore
 export class StreamingObject extends DurableObject {
     db: ReturnType<typeof drizzle>;
     deviceId: string | undefined;
